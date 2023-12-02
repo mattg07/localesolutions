bd3a65c000dea7445ee18f0bdb15c006623ad9a1
mattg07 <matiasmgodoy00@gmail.co.com>
2023-12-02 16:31:51 +0100

    addd

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "dist",
+    outDir: 'build', // Adjust this to your actual output directory
   },
   plugins: [react()],
 });
