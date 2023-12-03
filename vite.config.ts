7073fec14e18b49dce7e1ccff86f44140a4a99a6
mattg07 <matiasmgodoy00@gmail.co.com>
2023-12-03 14:46:35 +0100

    changesnew

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/localesolutions/",
   build: {
     outDir: 'build', // Adjust this to your actual output directory
   },
